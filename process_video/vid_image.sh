dc512fa0ada36fcf4d70d327a48f7ddb37096afe
PedroAlPacheco <pedro.pacheco.a@gmail.com>
2018-04-18 14:12:42 -0300

    Images ok

--- process_video/vid_image.sh
+++ process_video/vid_image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #ffmpeg -i Relaxinghighwaytraffic.avi -r 1 -s 100×100 -f image2 tmp-%01d.jpg
-ffmpeg -i Relaxinghighwaytraffic.avi -r 1 -f image2 tmp-%01d.jpg
+ffmpeg -i mis-cort.avi -r 1 -f image2 tmp-%01d.jpg
